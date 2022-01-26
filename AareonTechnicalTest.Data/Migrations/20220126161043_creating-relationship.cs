ee4bccb78ded70f2b33b68c504c04c5b4d338a1c
Simon Price <contractor.sprice@adelante.co.uk>
2022-01-26 16:20:38 +0000

    updating migrations

---
+++ AareonTechnicalTest.Data/Migrations/20220126161043_creating-relationship.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AareonTechnicalTest.Migrations
+{
+    public partial class creatingrelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_PersonId",
+                table: "Tickets",
+                column: "PersonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Persons_PersonId",
+                table: "Tickets",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Persons_PersonId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_PersonId",
+                table: "Tickets");
+        }
+    }
+}
